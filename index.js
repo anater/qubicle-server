73f08239d8fcb0edb0cb3ecd5f86719a7856c693
Andrew Nater <a.a.nater@gmail.com>
2016-12-30 10:14:34 -0500

    Add header. Update port.

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+app.use(setHeaders);
 app.use('/', function(req, res) {
     var url = 'http://sonar.corproot.com/api/timemachine/index' + req.url;
+    console.log(req.url);
     req.pipe(request(url)).pipe(res);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
+
+function setHeaders(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+}
